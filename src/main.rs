9cdf8fdf0ad08dd71caaac1010f297aec54efac4
hyperchessbot <hyperchessbot@gmail.com>
2021-01-17 14:34:51 +0000

    Measure elapsed time, add move overhead

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
 	env_logger::init();
 
 	let mut bot = LichessBot::new()
+		.uci_opt("Move Overhead", std::env::var("RUST_BOT_ENGINE_MOVE_OVERHEAD").unwrap_or("500".to_string()))
 		.uci_opt("Threads", std::env::var("RUST_BOT_ENGINE_THREADS").unwrap_or("4".to_string()))
 		.uci_opt("Hash", std::env::var("RUST_BOT_ENGINE_HASH").unwrap_or("128".to_string()))
 		.uci_opt("Contempt", std::env::var("RUST_BOT_ENGINE_CONTEMPT").unwrap_or("-25".to_string()))
--- startbot.sh
+++ startbot.sh
@@ -1,7 +1,6 @@
 chmod +x rustbot
 chmod +x stockfish12
-ls -l ./
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./
-echo "LD_LIBRARY_PATH"
-echo $LD_LIBRARY_PATH
+
 ./rustbot
