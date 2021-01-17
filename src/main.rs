5ef60c815f1cf60b4d20b61c3956bcc1decece5e
hyperchessbot <hyperchessbot@gmail.com>
2021-01-17 10:58:29 +0000

    Ponder

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ use lichessbot::lichessbot::*;
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
 	env_logger::init();
 
-	let mut bot = LichessBot::new();
+	let mut bot = LichessBot::new()
+		.uci_opt("Threads", std::env::var("RUST_BOT_ENGINE_THREADS").unwrap_or("4".to_string()))
+		.uci_opt("Hash", std::env::var("RUST_BOT_ENGINE_HASH").unwrap_or("128".to_string()))
+		.uci_opt("Contempt", std::env::var("RUST_BOT_ENGINE_CONTEMPT").unwrap_or("-25".to_string()))
+	;
 
 	bot.stream().await
 }
