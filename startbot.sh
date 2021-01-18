5e1ddcdf7ae936d86ff314b1cdd03d1ed40f3a4a
hyperchessbot <hyperchessbot@gmail.com>
2021-01-18 05:16:11 +0000

    Restart bot at regular intervals

--- startbot.sh
+++ startbot.sh
@@ -1,6 +1,18 @@
+#!/bin/bash
+
 chmod +x rustbot
 chmod +x stockfish12
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./
 
-./rustbot
+RESTART="${RESTART:-1800}"
+
+echo "restart period $RESTART"
+
+while true; do
+	echo "starting bot"
+	./rustbot &
+	sleep $RESTART
+	echo "shutting down bot"
+	kill $!
+done
